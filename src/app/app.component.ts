3273672786d25e67bb61c080dbdba8470f2c7599
Pedro Rios <st.pedro@gmail.com>
2020-04-16 13:00:43 -0500

    Part 00008 COMPLETE!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,9 +10,25 @@ import { Satellite } from './satellite';
 export class AppComponent {
   title = 'orbit-report';
   sourceList: Satellite[];
+  displayList: Satellite[];
   
+
+  search(searchTerm: string): void {
+    let matchingSatellites: Satellite[] = [];
+    searchTerm = searchTerm.toLowerCase();
+    for(let i=0; i < this.sourceList.length; i++) {
+       let name = this.sourceList[i].name.toLowerCase();
+       if (name.indexOf(searchTerm) >= 0) {
+          matchingSatellites.push(this.sourceList[i]);
+       }
+    }
+
+    this.displayList = matchingSatellites;
+ }
+
   constructor() {
     this.sourceList = [];
+    this.displayList = [];
     let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
  
     window.fetch(satellitesUrl).then(function(response) {
@@ -23,10 +39,13 @@ export class AppComponent {
             let satData=new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
             this.sourceList.push(satData);
           }
+          this.displayList = this.sourceList.slice(0);
        }.bind(this));
     }.bind(this));
  
  }
+
+
 }
 
 
