6362b2a57edf735ab49b47ddc7a8b94a44307202
Pedro Rios <st.pedro@gmail.com>
2020-04-16 10:21:16 -0500

    part 00004 COMPLETE

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ import { Satellite } from './satellite';
 export class AppComponent {
   title = 'orbit-report';
   sourceList: Satellite[];
+  
   constructor() {
     this.sourceList = [
        new Satellite("SiriusXM", "Communication", "2009-03-21", "LOW", true),
